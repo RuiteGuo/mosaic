8fe6a35387c36e0d3f2472d43d0f7825e2ecfee7
Ruite Guo <rguo34@wisc.edu>
2018-03-18 17:24:05 -0500

    debugged the homography computation, verified H matrix

--- computeHomography.m
+++ computeHomography.m
@@ -3,30 +3,39 @@ function H_3x3 = computeHomography(src_pts_nx2, dest_pts_nx2)
 
 [rows,cols] = size (src_pts_nx2);
 
-linearSystem = zeros (rows*2,9);
+linearSystem = zeros (rows*2 + 1,9);
 H = zeros(9,1);
-solutions = zeros(9,1);
-solutions(9,1) = 1;
+solutions = zeros(rows*2 + 1,1);
 
+% prevent obvious solutions for all 0
+linearSystem (rows*2 +1 , 9) = 1;
+solutions (rows*2+1,1) = 1;
 
-
-for k = 1:rows
+n = 1;
+for k = 1:rows 
    
+
     xs=src_pts_nx2(k,1);
     ys=src_pts_nx2(k,2);
     xd= dest_pts_nx2(k,1);
     yd= dest_pts_nx2(k,2);
-   linearSystem(k,1:3) = [-xs,-ys,-1];
-   linearSystem(k,4:6) = 0;
-   linearSystem(k,7:9) = [xs*xd,ys*xd,xd];
-
-   linearSystem(k+1,4:6) = [-xs,-ys,-1];
-   linearSystem(k+1,1:3) = 0;
-   linearSystem(k+1,7:9) = [xs*yd,ys*yd,yd];
+   linearSystem(n,1:3) = [-xs,-ys,-1];
+   linearSystem(n,4:6) = 0;
+   linearSystem(n,7:9) = [xs*xd,ys*xd,xd];
+    
+ 
+   linearSystem(n+1,4:6) = [-xs,-ys,-1];
+   linearSystem(n+1,1:3) = 0;
+   linearSystem(n+1,7:9) = [xs*yd,ys*yd,yd];
+    n = n+2;
 end
-% solve through least square 
 
-H = lsqnonneg(linearSystem,solutions);
+linearSystem
+
+% solve through least square
+trans = transpose(linearSystem);
+
+H = inv(trans*linearSystem)*trans*solutions;
 
 H_3x3 = zeros(3,3);
 H = transpose(H);
